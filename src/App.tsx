00cc4c9f6113ac583d74b07a0cbd277c3230e18b
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-12-26 22:11:07 +0200

    nail project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Photo from "./pages/Photo";
+import Price from "./pages/Price";
+import Contact from "./pages/Contact";
+import Footer from "./pages/Footer";
+import HomeMobile from "./pages/HomeMobile";
+import "./styles/global.scss";
+import "./App";
+
+import { useMediaQuery } from "react-responsive";
+
 
 function App() {
+  const isMobile = useMediaQuery({ query: `(max-width: 390px)` });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body__page">
+      {isMobile ? <HomeMobile /> : <Home />}
+      <Photo img={""} />
+      <Price />
+      <Contact />
+      <Footer name={""} link={""} />
     </div>
   );
 }
